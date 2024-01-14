8a7f50a12e775e32c199959ffb13e3af401a0c56
Adryan1928 <adryanoliveira1928@gmail.com>
2024-01-14 16:00:25 -0300

    A primeira tela está funcionando

---
+++ JavaScript/Pokedex/assets/js/main.js
@@ -0,0 +1,43 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMore')
+const maxRecords = 20
+const limit = 10
+let offset = 0
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    })   
+}
+
+loadPokemonItems(offset, limit)
+
+loadMoreButton.addEventListener('click', () =>{
+    offset += limit
+
+    const qtdRecord = offset + limit
+
+    if (qtdRecord >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItems(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItems(offset, limit)
+    }
+})

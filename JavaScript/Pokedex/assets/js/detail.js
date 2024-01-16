101b3e8237a0ab94954df9da90b6d58c84479719
Adryan1928 <adryanoliveira1928@gmail.com>
2024-01-16 19:48:46 -0300

    Pokedex concluída

--- JavaScript/Pokedex/assets/js/detail.js
+++ JavaScript/Pokedex/assets/js/detail.js
@@ -1,12 +1,35 @@
 const ContentTypes = document.getElementById('types')
 const ContentName = document.getElementsByTagName('h1')[0]
+const Contentnumber = document.getElementById('number')
+const ContentHeader = document.getElementById('header')
+const ContentImg = document.getElementById('img')
+const ContentInfoTypes = document.getElementById('types-info')
 
 function getPokemon(){
-    
+    return JSON.parse(localStorage.getItem('pokemon'))
+}
+
+function addImg(url){
+    ContentImg.innerHTML = `<img src="${url}">`
 }
 
 function main(){
+    let pokemon = getPokemon()
+    ContentName.innerText = pokemon.name
+    Contentnumber.innerText = '#'+pokemon.number
+    ContentHeader.classList.add(pokemon.type)
+    addImg(pokemon.photo)
 
+    let i = 0
+    for (type of pokemon.types) {
+        ContentTypes.innerHTML += `<p class='${type}'>${type}</p>`
+        if (i == 0)
+            ContentInfoTypes.innerHTML += `<div class="linha types"><span class="opacity">Principal</span><span class='color-${type}'>${type}</span></div>`
+        else
+            ContentInfoTypes.innerHTML += `<div class="linha types"><span class="opacity">Outros</span><span class='color-${type}'>${type}</span></div>`
+        i++
+    }
+    
 }
 
 main()

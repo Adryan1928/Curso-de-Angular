b05bc41dca60fb305c6db1c5b68db6392f716083
Adryan1928 <adryanoliveira1928@gmail.com>
2024-01-16 12:18:23 -0300

    Quase tudo concluído

---
+++ JavaScript/Pokedex/assets/js/detail.js
@@ -0,0 +1,12 @@
+const ContentTypes = document.getElementById('types')
+const ContentName = document.getElementsByTagName('h1')[0]
+
+function getPokemon(){
+    
+}
+
+function main(){
+
+}
+
+main()
--- JavaScript/Pokedex/assets/js/main.js
+++ JavaScript/Pokedex/assets/js/main.js
@@ -4,9 +4,16 @@ const maxRecords = 20
 const limit = 10
 let offset = 0
 
+function irParaDetail(pokemon) {
+    localStorage.setItem('pokemon', JSON.stringify(pokemon))
+
+    window.location.href = './detail.html'
+
+}
+
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" id="${pokemon.number}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
@@ -22,7 +29,10 @@ function convertPokemonToLi(pokemon) {
 function loadPokemonItems(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-    })   
+        pokemons.map((pokemon) => {
+            document.getElementById(`${pokemon.number}`).addEventListener('click', () => irParaDetail(pokemon))
+        })
+    })
 }
 
 loadPokemonItems(offset, limit)
